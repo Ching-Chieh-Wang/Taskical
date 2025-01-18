3bd890fc9b2bd9573863ea53ae4ec182d8e8a17c
捷 <immactavish@gmail.com>
2025-01-18 18:26:27 -0500

    fix: remove edit and delete from task detail tab
    - no such requirement

--- components/TaskActionNav.tsx
+++ components/TaskActionNav.tsx
@@ -2,7 +2,6 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import EditIcon from '@/components/icons/EditIcon';
 import { useTaskContext } from '@/context/TaskContext';
-import DeleteIcon from './icons/DeleteIcon';
 
 const TaskActionNav = ({ index }: { index: number }) => {
   const { actions } = useTaskContext(); // Access actions from context
@@ -11,17 +10,10 @@ const TaskActionNav = ({ index }: { index: number }) => {
     actions.startEditingTask(index); // Trigger editing action for the task
   };
 
-  const handleDelete = () => {
-    actions.deleteTask(index); // Trigger deleteTask action for the task
-  };
-
   return (
     <View className="flex-row items-center space-x-4">
       <TouchableOpacity onPress={handleEdit} className="p-2 rounded-full bg-gray-100">
         <EditIcon  />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handleDelete} className="p-2 rounded-full bg-gray-100">
-        <DeleteIcon color="#FF6B6B"/>
       </TouchableOpacity>
     </View>
   );
